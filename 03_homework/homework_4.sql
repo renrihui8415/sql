728172276d731229ddedc8238e83bfe42ce32072
Julia Ren <121738450+renrihui8415@users.noreply.github.com>
2024-05-21 21:37:30 -0400

    Answers to homework 4

--- 03_homework/homework_4.sql
+++ 03_homework/homework_4.sql
@@ -17,8 +17,9 @@ The `||` values concatenate the columns into strings.
 Edit the appropriate columns -- you're making two edits -- and the NULL rows will be fixed. 
 All the other rows will remain the same.) */
 
-
-
+SELECT 
+    COALESCE(product_name, '') || ', ' || COALESCE(product_size,'') || ' (' || COALESCE(product_qty_type, 'unit') || ')' as Product_List
+FROM product;
 
 --Windowed Functions
 /* 1. Write a query that selects from the customer_purchases table and numbers each customer’s  
@@ -30,11 +31,91 @@ each new market date for each customer, or select only the unique market dates p
 (without purchase details) and number those visits. 
 HINT: One of these approaches uses ROW_NUMBER() and one uses DENSE_RANK(). */
 
+-- Approach 1, row_number 
+-- Select only the unique market dates per customer
+SELECT 
+	customer_id,
+    market_date,
+	row_number() OVER (PARTITION BY customer_id ORDER BY market_date) as visit_number
+FROM (
+
+	SELECT 
+		DISTINCT customer_id, market_date 
+	FROM customer_purchases
+	
+) as distinct_visits;
+		
+-- Approach 2, dense_rank 
+-- Display all rows in the 'customer_purchases' table
+SELECT
+	*,
+	dense_rank() OVER (PARTITION BY customer_id ORDER BY market_date) as visit_number
+
+FROM customer_purchases;
+
+
 
 /* 2. Reverse the numbering of the query from a part so each customer’s most recent visit is labeled 1, 
 then write another query that uses this one as a subquery (or temp table) and filters the results to 
 only the customer’s most recent visit. */
 
+-- Reverse the rank in 'DESC' order
+-- Select only the unique market dates per customer to ensure a single recent visit date per customer
+-- Otherwise, one customer may have duplicate recent visit dates
+SELECT
+	customer_id,
+    market_date,
+	dense_rank() OVER (PARTITION BY customer_id ORDER BY market_date DESC) as visit_number
+FROM(
+	SELECT 
+		DISTINCT customer_id, market_date 
+	FROM customer_purchases
+) as distinct_visits;
+
+-- SELECT the most recent visit for each customer
+SELECT 
+	customer_id,
+	market_date as recent_visit
+FROM (
+
+		SELECT
+			customer_id,
+			market_date,
+			dense_rank() OVER (PARTITION BY customer_id ORDER BY market_date DESC) as visit_number
+		FROM(
+			SELECT 
+				DISTINCT customer_id, market_date 
+			FROM customer_purchases
+		) as distinct_visits
+	
+) AS rank_visits
+WHERE 
+	rank_visits.visit_number=1;
 
 /* 3. Using a COUNT() window function, include a value along with each row of the 
 customer_purchases table that indicates how many different times that customer has purchased that product_id. */
+
+-- Approach 1 
+-- Display all rows in the 'customer_purchases' table 
+-- with 'purchase_times' along with each row 
+SELECT 
+	*,
+	count(*) OVER (PARTITION BY customer_id, product_id) as purchase_times
+FROM 
+	customer_purchases;
+
+-- Approach 2
+-- Select only the unique product_ids per customer without purchase details.
+SELECT
+	DISTINCT customer_id, product_id, purchase_times
+FROM (
+		SELECT 
+			customer_id,
+			product_id,
+			count(*) OVER (PARTITION BY customer_id, product_id) as purchase_times
+		FROM 
+			customer_purchases
+) AS x;
+
+
+	
